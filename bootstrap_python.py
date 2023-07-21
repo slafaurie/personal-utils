21bc99ac90ad8a6ec0dcdf6c5fc502a803ecc0da
slafaurie <102782372+slafaurie@users.noreply.github.com>
2023-07-21 15:55:10 +0200

    Update bootstrap_python.py

--- bootstrap_python.py
+++ bootstrap_python.py
@@ -121,3 +121,82 @@ def comprehension_list_bootstrap(data:pd.DataFrame, column:str, B:int, CIs:list[
     means = [data.iloc[bootstrap_idx[x]][column].mean() for x in range(B)]
     stats = calculate_CIs(means)
     return stats
+
+
+def scalable_vectorize_bootstrap(df: pd.DataFrame, column: str, granularity: list[str], B: int, CIs: list[int] = [2.5, 97.5]) -> pd.DataFrame:
+    """
+    Perform a scalable Bootstrap resampling using vectorization for statistical inference on a DataFrame.
+
+    Args:
+        df (pd.DataFrame): The input DataFrame containing the data to be resampled.
+        column (str): The name of the column in 'df' for which the bootstrap resampling will be performed.
+        granularity (list[str]): A list of columns that act as a partition for the bootstrap algorithm.
+        B (int): The number of bootstrap iterations to perform.
+        CIs (list[int], optional): A list of two values representing the lower and upper percentiles
+                                   for the confidence interval. Defaults to [2.5, 97.5].
+
+    Returns:
+        pd.DataFrame: A DataFrame containing Bootstrap statistics for each group specified in 'granularity'.
+
+    Description:
+        The scalable_vectorize_bootstrap function performs a Bootstrap resampling on the
+        specified column of the input DataFrame 'df' for each group specified in the 'granularity' list.
+        The Bootstrap resampling technique is used to estimate the sampling distribution of a statistic
+        (in this case, the mean) by repeatedly resampling with replacement from the original data.
+
+        The function leverages Numpy's broadcasting and vectorization capabilities to efficiently handle
+        large datasets. It first calculates random bootstrap indices for each group using the 'groupby'
+        and 'apply' methods. The 'broadcast_values' are obtained for each group by using 'groupby' and 'agg'
+        methods to combine the values of the specified 'column' into a list. These lists are then
+        broadcasted to a (B, N) array where B is the number of bootstrap iterations, and N is the size
+        of the original group.
+
+        The function computes the Bootstrap means for each group by using advanced indexing with Numpy,
+        selecting values from the broadcasted arrays using the random indices. It then calculates the
+        means along the samples axis to obtain a (B, ) array of Bootstrap means for each group.
+
+        Finally, the function calculates the confidence intervals for the Bootstrap means by using the
+        'calculate_CIs' function. The results are returned in a DataFrame containing the group identifiers
+        specified in 'granularity', along with the Bootstrap statistics including mean, lower CI, and upper CI.
+    """
+    
+    # get the indexes for the bootstrap
+    idx = (
+        df
+        .groupby(granularity, as_index=False)
+        .size()
+        .assign(
+            random_idx = lambda df_: df_.apply(lambda x: np.random.randint(0, x["size"], (B, x["size"])), axis=1)
+        )
+    )
+
+    # get the values for the bootstrap
+    groups = (
+        df
+        .groupby(granularity, as_index=False)
+        .agg(
+            broadcast_values = (column, list)
+        )
+        .assign(
+            broadcast_values = lambda df_: df_["broadcast_values"].apply(lambda x: np.tile(x, (B,1))) 
+        )
+    )
+
+    bootstraps_result = (
+        idx
+        .merge(groups, on=granularity)
+        .assign(
+            means = lambda df_: df_.apply(lambda x: x["broadcast_values"][np.arange(B)[:, np.newaxis], x["random_idx"]].mean(axis=1), axis=1)
+            , stats = lambda df_: df_.apply(lambda x: calculate_CIs(x["means"], CIs).asdict(), axis=1)
+        )
+        .apply(lambda x: pd.Series(x["stats"]), axis=1)
+    )
+
+    final_table = (
+        idx
+        .drop(columns=["random_idx", "size"])
+        .merge(bootstraps_result, left_index=True, right_index=True)
+    )
+
+    return final_table
+
